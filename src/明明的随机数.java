3476fef653c36c133a6e497ad4883e1b4b265809
linyihan <1032612468@qq.com>
2017-03-18 18:16:35 +0800

    2017/3/18 18:10

--- src/明明的随机数.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class 明明的随机数 {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		String string = scanner.nextLine().toLowerCase();
-	}
-}
