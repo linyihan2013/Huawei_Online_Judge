684e296e2a7a218b2afacb0926a8c1aadd01b353
linyihan <1032612468@qq.com>
2017-03-30 21:45:43 +0800

    2017/3/30 21:45 中级题也做完了

--- src/高级/火车进站.java
+++ src/高级/火车进站.java
@@ -1,19 +1,22 @@
 package 高级;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class 火车进站 {
 	public static List<List<Integer>> perms = new ArrayList<List<Integer>>();
-	
+
 	public static void permutation(List<Integer> input, List<Integer> result) {
 		if (result.size() == input.size()) {
 			List<Integer> perm = new ArrayList<Integer>();
 			for (Integer integer : result) {
 				perm.add(integer);
 			}
-			
+
 			perms.add(perm);
 		} else {
 			for (int i = 0; i < input.size(); i++) {
@@ -25,22 +28,74 @@ public class 火车进站 {
 			}
 		}
 	}
-	
+
+	public static void print(List<Integer> inList, List<Integer> outList) {
+		Stack<Integer> stack = new Stack<Integer>();
+
+		boolean flag = true;
+		int inIndex = 0, outIndex = 0;
+		while ((inIndex < inList.size()) && (outIndex < outList.size())) {
+			if ((!stack.isEmpty()) && (stack.peek() == outList.get(outIndex))) {
+				stack.pop();
+				outIndex++;
+			} else if (outList.get(outIndex) != inList.get(inIndex)) {
+				stack.push(inList.get(inIndex));
+				inIndex++;
+			} else {
+				inIndex++;
+				outIndex++;
+			}
+		}
+
+		if (outIndex < outList.size()) {
+			while (!stack.isEmpty()) {
+				if (stack.peek() != outList.get(outIndex)) {
+					flag = false;
+					break;
+				} else {
+					stack.pop();
+					outIndex++;
+				}
+			}
+		}
+
+		if (flag) {
+			for (int i = 0; i < outList.size(); i++) {
+				if (i > 0) {
+					System.out.print(" ");
+				}
+
+				System.out.print(outList.get(i));
+			}
+			System.out.println();
+		}
+	}
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int num = scanner.nextInt();
-		List<Integer> input = new ArrayList<Integer>();
-		
-		while (num-- > 0) {
-			int i = scanner.nextInt();
-			input.add(i);
-		}
-		
-		List<Integer> result = new ArrayList<Integer>();
-		permutation(input, result);
-		
-		for (List<Integer> list : perms) {
-			System.out.println(list);
+
+		while (scanner.hasNextInt()) {
+			int num = scanner.nextInt();
+			List<Integer> inList = new ArrayList<Integer>();
+			int[] array = new int[num];
+
+			for (int i = 0; i < num; i++) {
+				int in = scanner.nextInt();
+				inList.add(in);
+				array[i] = in;
+			}
+
+			Arrays.sort(array);
+			List<Integer> input = new ArrayList<Integer>();
+			for (int i = 0; i < num; i++) {
+				input.add(array[i]);
+			}
+			List<Integer> result = new ArrayList<Integer>();
+			permutation(input, result);
+
+			for (List<Integer> outList : perms) {
+				print(inList, outList);
+			}
 		}
 	}
 }
